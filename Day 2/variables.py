628ad16ff16633a3903955e54d4f6141902efc7c
Nazeer0943 <nazeer0943@gmail.com>
2024-11-26 22:50:38 +0100

    Assignment 1 succesfully done. Assignment 1 succesfully done.

---
+++ Day 2/variables.py
@@ -0,0 +1,13 @@
+#'Day 2: 30 Days of python programming'
+first_name = "Nazir"
+last_name = 'Sani'
+full_name = "Nazir Sani"
+country = "Nigeria"
+city = 'Katsina'
+age = 25
+year = 2024
+is_married = "yes"
+is_true = "yes"
+is_light = 'yes'
+a, b, c = 1, 2, 3
+print(a, b, c)
