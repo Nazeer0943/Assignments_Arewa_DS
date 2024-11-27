74f22460c1e592053b5207aa42a0e7fccd262cd1
Nazeer0943 <nazeer0943@gmail.com>
2024-11-27 22:33:40 +0100

    Attempting assignment 2

--- Day 2/variables.py
+++ Day 2/variables.py
@@ -11,3 +11,35 @@ is_true = "yes"
 is_light = 'yes'
 a, b, c = 1, 2, 3
 print(a, b, c)
+#CHECKING THE DATA TYPE OF ALL THE VARIABLES CREATED USING THE BUILT-IN FUNCTION
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light))
+# Finding the length of my first name
+print(len(first_name))
+# comapring the lenght with my last name
+print(len(last_name))
+#declaring numbers
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+reminder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+#calculating the area of a cirscle and its circumference
+radius = 30
+PI = 3.14
+area_of_a_circle = PI * radius **2
+circum_of_circle = 2*PI*radius
+print(area_of_a_circle)
+print(circum_of_circle)
